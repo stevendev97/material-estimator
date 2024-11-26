34444215035b92121c4245647437f4ddf7e195fb
stevendev97 <x.limsteven@gmail.com>
2024-11-26 00:02:17 -0600

    Prepare for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: '/material-estimator/',
+  basePath: '/material-estimator',
+  images: {
+    unoptimized: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
